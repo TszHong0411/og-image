1a3c8198724bfc8ec840bc04cb8adfb37f6edc63
tszhong0411 <lainelson411@gmail.com>
2023-04-04 11:12:03 +0800

    fix: 用相對路徑

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata = {
     index: true,
     follow: true,
   },
-  manifest: `${site.url}/static/favicon/site.webmanifest`,
+  manifest: '/static/favicon/site.webmanifest',
   twitter: {
     title: site.name,
     card: 'summary_large_image',
